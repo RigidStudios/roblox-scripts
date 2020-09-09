24536f79c30e8c94a74d952eb667814b7db5eef8
CDTtrix <dtwatson443@gmail.com>
2020-09-09 11:51:16 -0500

    Changed comments and added an api reference
    Added an api reference for nuubs, and made comments make sense.

--- modules/math_parser.lua
+++ modules/math_parser.lua
@@ -1,6 +1,25 @@
+-- Author Rigid
+-- Date 09/03/20
+
 local mather = {};
 
--- LuaPatterns for numbers and operations.
+--[[ 
+Math Parser Api:
+
+	_math:GetNumbers(string) -->> {numbers}
+	_math.Add(string) -->> ?results and ?remaining | ?number
+	_math.Subtract(string) -->> ?results and ?remaining | ?number
+	_math.Percent(string) -->> ?results and ?remaining | ?number
+	_math.Divide(string) -->> ?results and ?remaining | ?number
+	_math.Multiply(string) -->> ?results and ?remaining | ?number
+	_math.FormatMultiply(string) -->> string
+	_math.Parentheses(string) -->> ?results and ?remaining | ?number
+	_math:Caculate(string) -->> string
+]]
+
+
+
+-- .. LuaPatterns for numbers and operations.
 local patterns = {};
 patterns.num     = "%-?%d+%.?%d*";
 patterns.mult    = "%s-%*%s-";
@@ -11,8 +30,8 @@ patterns.minus   = "%s-%-%s-";
 patterns.div     = "%s-%/%s-";
 patterns.parentheses = "%b()"
 
--- Extract numbers from string where possible.
-function mather:GetNums(str)
+-- .. Extract numbers from string where possible.
+function mather:GetNumbers(str)
 	local numbers = {};
 	for number in str:gmatch(patterns.num) do
 		table.insert(numbers, tonumber(number));
@@ -21,59 +40,59 @@ function mather:GetNums(str)
 end
 
 
--- PLUS (+)
-function mather.plus(str)
+-- ADD (+)
+function mather.Add(str)
 	local res, remaining = str:gsub(patterns.num..patterns.plus..patterns.num, function(secondStr)
-		local newNums = mather:GetNums(secondStr);
+		local newNums = mather:GetNumbers(secondStr);
 		return newNums[1] + newNums[2];
 	end);
 	return res, remaining;
 end
 
 -- MINUS (-)
-function mather.minus(str)
+function mather.Subtract(str)
 	local res, remaining = str:gsub(patterns.num..patterns.minus..patterns.num, function(secondStr)
 		-- Remove the matched string minus to not consider negative: 4/9/20
-		local newNums = mather:GetNums(secondStr:gsub("("..patterns.num.."%s-)(%-)(%s-"..patterns.num..")", function(a, b, c) print(a, b, c) return a.." "..c end));
+		local newNums = mather:GetNumbers(secondStr:gsub("("..patterns.num.."%s-)(%-)(%s-"..patterns.num..")", function(a, b, c) print(a, b, c) return a.." "..c end));
 		return newNums[1] - newNums[2];
 	end);
 	return res, remaining;
 end
 
 -- PERCENT (%)
-function mather.percent(str)
+function mather.Percent(str)
 	local res, remaining = str:gsub(patterns.num..patterns.percent, function(secondStr)
-		local newNums = mather:GetNums(secondStr);
+		local newNums = mather:GetNumbers(secondStr);
 		return newNums[1] / 100;
 	end);
 	return res, remaining;
 end
 
 -- DIVISION (/)
-function mather.div(str)
+function mather.Divide(str)
 	local res, remaining = str:gsub(patterns.num..patterns.div..patterns.num, function(secondStr)
-		local newNums = mather:GetNums(secondStr);
+		local newNums = mather:GetNumbers(secondStr);
 		return newNums[1] / newNums[2];
 	end)
 	return res, remaining;
 end
 
 -- MULTIPLICATION (*)
-function mather.mult(str)
+function mather.Multiply(str)
 	local res, remaining = str:gsub(patterns.num..patterns.mult..patterns.num, function(secondStr)
-		local newNums = mather:GetNums(secondStr);
+		local newNums = mather:GetNumbers(secondStr);
 		return newNums[1] * newNums[2];
 	end)
 	return res, remaining;
 end
 
--- MULTIPLICATION (x)
-function mather.mult2(str)
-	return mather.mult(str:gsub("x", "*"));
+-- Format (x)
+function mather.FormatMultiply(str)
+	return mather.Multiply(str:gsub("x", "*"));
 end
 
 -- PARENTHESES ((...))
-function mather.parentheses(str)
+function mather.Parentheses(str)
 	local res, remaining = str:gsub(patterns.parentheses, function(secondStr)
 		-- Remove parentheses to re-calculate.
 		secondStr = secondStr:sub(2, -2);
@@ -86,15 +105,15 @@ end
 function mather:Calculate(str)
 	-- Patterns to perform.
 	local usablePatterns = {
-		mather.parentheses;
+		mather.Parentheses;
 		
-		mather.percent;
-		mather.mult;
-		mather.mult2;
-		mather.div;
+		mather.Percent;
+		mather.Multiply;
+		mather.FormatMultiply;
+		mather.Divide;
 		
-		mather.minus;
-		mather.plus;
+		mather.Subtract;
+		mather.Add;
 	};
 	
 	local remainingOperations = 0;
