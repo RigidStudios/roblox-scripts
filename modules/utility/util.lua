222bd66195ca92086f4b776b9d65a05d1502b1d3
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-11-10 20:54:01 +0100

    feat(util): reverse table indexof
    C-Based indexing operation quicker than value search. `break` removed as it was after a `return`.

--- modules/utility/util.lua
+++ modules/utility/util.lua
@@ -7,13 +7,12 @@ local utils = {};
       ["Hello"] = function() print("Hello to you too!") end; -- This executes
     }
 --]]
-utils.switch = function(condition)
+utils.switch = function(condition, ...)
   return function(cases)
     for check, func in pairs(cases) do
       if check == condition and func then
-        local success, err = pcall(func);
+        local success, err = pcall(func, ...);
         return success;
-        break;
       end;
     end;
   end;
@@ -26,11 +25,25 @@ end);
       {"Hey", "there!"}
 --]]
 utils.splitToWords = function(str)
-    local t = {};
-    for i in str:gmatch("%S+") do
-        table.insert(t, i);
-    end;
-    return t;
+  local t = {};
+  for i in str:gmatch("%S+") do
+      table.insert(t, i);
+  end;
+  return t;
+end;
+
+--[[
+  USAGE:
+    _utils.getIndexOf({"Hello", "Good afternoon...", 25}, 25)
+    -- Output:
+      3
+--]]
+utils.getIndexOf = function(tab, value)
+  local reverseTable = {};
+  for key, value in pairs(values) do
+    reverseTable[value] = key;
+  end;
+  return reverseTable[value];
 end;
 
 return utils;
