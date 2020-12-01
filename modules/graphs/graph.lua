4744f275a1d69a4458260f10f88039b49e45b5e8
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-12-01 20:53:29 +0100

    docs(graphs): beginning of documentation

--- modules/graphs/graph.lua
+++ modules/graphs/graph.lua
@@ -5,7 +5,13 @@ graph.__index = graph
 graph.Edge = {}
 graph.Edge.__index = graph.Edge
 
-function graph.Edge.create(v, w, weight)
+--!strict
+
+-- graph.Edge.create - Create a new edge between:
+-- @params <any> v
+-- @params <any> w
+-- @params <number?> weight - Determines weight in shortest-path considerations.
+function graph.Edge.create(v, w, weight: number?)
     local s = {}
     setmetatable(s, graph.Edge)
 
@@ -20,18 +26,23 @@ function graph.Edge.create(v, w, weight)
     return s
 end
 
+-- graph.Edge:from() - Returns origin of edge
 function graph.Edge:from()
     return self.v
 end
 
+-- graph.Edge:to() - Returns end of edge
 function graph.Edge:to()
     return self.w;
 end
 
+-- graph.Edge:either() - Returns origin of edge
 function graph.Edge:either()
     return self.v
 end
 
+-- graph.Edge:other() - Returns edge omitted from:
+-- @params <any> x - Other Vertice ignored.
 function graph.Edge:other(x)
     if x == self.v then
         return self.w
@@ -41,36 +52,39 @@ function graph.Edge:other(x)
 
 end
 
-function graph.create(V, directed)
-    local g = {}
-    setmetatable(g, graph)
-
-    if directed == nil then
-        directed = false
-    end
-
-    g.vertexList = require('luagraphs.data.list').create()
+-- graph.create - Create a new graph from:
+-- @params <number> V - Amount of vertices to create.
+-- @params <boolean?> directed
+function graph.create(V, directed: boolean?)
+    local g = setmetatable({}, graph)
+    
+    g.vertexList = require(script.list).create()
     g.adjList = {}
     for v = 0,V-1 do
         g.vertexList:add(v)
-        g.adjList[v] = require('luagraphs.data.list').create()
+        g.adjList[v] = require(script.list).create()
     end
-    g.directed = directed
+    g.directed = directed ~= nil and true or false
 
     return g
 end
 
+-- graph:vertexCount() - Returns amount of vertices in current graph.
 function graph:vertexCount()
     return self.vertexList:size()
 end
 
+-- graph:vertexCount() - Returns vertices in current graph.
 function graph:vertices()
     return self.vertexList
 end
 
-function graph.createFromVertexList(vertices, directed)
-    local g = {}
-    setmetatable(g, graph)
+-- graph.createFromVertexList - Create a new graph from:
+-- @params <any[]> V - Vertices to create.
+-- @params <boolean?> directed
+function graph.createFromVertexList(vertices, directed: boolean?)
+    local g = setmetatable({}, graph)
+    
 
     if directed == nil then
         directed = false
@@ -80,23 +94,27 @@ function graph.createFromVertexList(vertices, directed)
     g.adjList = {}
     for i = 0,g.vertexList:size()-1 do
         local v = g.vertexList:get(i)
-        g.adjList[v] = require('luagraphs.data.list').create()
+        g.adjList[v] = require(script.list).create()
     end
-    g.directed = directed
+    g.directed = directed ~= nil and true or false
 
     return g
 end
 
+-- graph:addVertexIfNotExists() - Add a vertice if it doesn't exist in graph:
+-- @params <any> v - Vertice to append.
 function graph:addVertexIfNotExists(v)
     if self.vertexList:contains(v) then
         return false
     else
         self.vertexList:add(v)
-        self.adjList[v] = require('luagraphs.data.list').create()
+        self.adjList[v] = require(script.list).create()
         return true
     end
 end
 
+-- graph:removeVertex - Remove a vertice from graph:
+-- @params <any> v - Vertice to remove.
 function graph:removeVertex(v)
     if self.vertexList:contains(v) then
         self.vertexList:remove(v)
@@ -118,15 +136,21 @@ function graph:removeVertex(v)
     end
 end
 
+-- graph.createFromVertexList - Create a new graph from:
+-- @params <any> v - Vertice to check for.
 function graph:containsVertex(v)
     return self.vertexList:contains(v)
 end
 
+-- graph:adj()
+-- @params <any> v
 function graph:adj(v)
     return self.adjList[v]
 end
 
-function graph:addEdge(v, w, weight)
+-- graph:addEdge() - Create a new graph from:
+-- @params <any[]> V - Vertices to 
+function graph:addEdge(v, w, weight: number?)
     local e = graph.Edge.create(v, w, weight)
     self:addVertexIfNotExists(v)
     self:addVertexIfNotExists(w)
